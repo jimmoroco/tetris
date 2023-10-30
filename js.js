694d318a18780d13fd34eb1041e957baa6cd9539
Jim Moroco <jim.moroco@gmail.com>
2023-10-29 22:43:14 -0500

    feat: modify canvas

--- js.js
+++ js.js
@@ -3,7 +3,9 @@ let context;
 
 const BLOCK_SIZE = 20;
 const CANVAS_WIDTH = 14;
-const CANVAS_HEIGHT = 35;
+const CANVAS_HEIGHT = 25;
+
+let board = [];
 
 window.onload = function () {
     initializeCanvas();
@@ -16,4 +18,15 @@ function initializeCanvas() {
     canvas.width = BLOCK_SIZE * CANVAS_WIDTH;
     canvas.height = BLOCK_SIZE * CANVAS_HEIGHT;
     context.scale(BLOCK_SIZE, BLOCK_SIZE);
+    generateBoard();
+}
+
+function generateBoard() {
+    let row = [];
+    for (let i = 0; i < CANVAS_HEIGHT; i++) {
+        for (let j = 0; j < CANVAS_WIDTH; j++) {
+            row.push(0);
+        }
+        board.push(row);
+    }
 }
